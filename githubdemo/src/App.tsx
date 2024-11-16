bcd8d427c21e4fa5e484d5576ccaa03836310c76
feter <feter_liang@hotmail.com>
2024-11-17 07:36:11 +0800

    x

--- githubdemo/src/App.tsx
+++ githubdemo/src/App.tsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ViteLogo from '/vite.svg'
 import './App.css'
 
-function App(8) {
+function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={ViteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+
         </a>
       </div>
       <h1>Vite + React</h1>
