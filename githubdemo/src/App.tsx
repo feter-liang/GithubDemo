a9c8ddd0188cdf033b0f6b093cb911a6cf7a12a3
feter <feter_liang@hotmail.com>
2024-11-16 17:23:44 +0800

    4

--- githubdemo/src/App.tsx
+++ githubdemo/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App(3) {
+function App(4) {
   const [count, setCount] = useState(0)
 
   return (
