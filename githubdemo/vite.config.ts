a7d9c9781379ca38004686ff935488ad9898bca6
feter <feter_liang@hotmail.com>
2024-11-18 11:20:05 +0800

    x

--- githubdemo/vite.config.ts
+++ githubdemo/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/githubdemo/',
-  server: {
-    port: 5173,
-    strictPort: true
-  },
+  base: '/githubdemo/',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
-}))
+})
